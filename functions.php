445ed0efb7c65318c783696f3ce6d461713d6e7e
Aaron Snowberger <jekkilekki@gmail.com>
2020-02-04 14:52:00 +0900

    [2.5] Add theme support in functions.php

--- functions.php
+++ functions.php
@@ -1,9 +1,245 @@
 <?php
+/**
+ * EdxStarter functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage EdxStarter
+ * @since 1.0.0
+ */
 
 /**
- * Enqueue Styles
+ * Table of Contents:
+ * Theme Support
+ * Required Files
+ * Register Styles
+ * Register Scripts
+ * Register Menus
+ * Custom Logo
+ * WP Body Open
+ * Register Sidebars
+ * Enqueue Block Editor Assets
+ * Enqueue Classic Editor Styles
+ * Block Editor Settings
  */
-function add_theme_scripts() {
-	wp_enqueue_style( 'edxstarter-style', get_stylesheet_uri(), array(), '20200125' );
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function edxstarter_theme_support() {
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	// Custom background color.
+	add_theme_support(
+		'custom-background',
+		array(
+			'default-color' => 'f5efe0',
+		)
+	);
+
+	// Set content-width.
+	// global $content_width;
+	// if ( ! isset( $content_width ) ) {
+	// 	$content_width = 580;
+	// }
+
+	/*
+	 * Enable support for Post Thumbnails on posts and pages.
+	 *
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
+
+	// Set post thumbnail size.
+	set_post_thumbnail_size( 1200, 9999 );
+
+	// Add custom image size used in Cover Template.
+	// add_image_size( 'edxstarter-fullscreen', 1980, 9999 );
+
+	add_theme_support(
+		'custom-logo',
+		array(
+			'height'      => 90,
+			'width'       => 120,
+			'flex-height' => true,
+			'flex-width'  => true,
+		)
+	);
+
+	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'script',
+			'style',
+		)
+	);
+
+	/*
+	 * Make theme available for translation.
+	 * Translations can be filed in the /languages/ directory.
+	 * If you're building a theme based on EdxStarter, use a find and replace
+	 * to change 'edxstarter' to the name of your theme in all the template files.
+	 */
+	load_theme_textdomain( 'edxstarter' );
+
+	// Add support for full and wide align images.
+	add_theme_support( 'align-wide' );
+
+	// Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
+
 }
-add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+add_action( 'after_setup_theme', 'edxstarter_theme_support' );
+
+/**
+ * REQUIRED FILES
+ * Include required files.
+ */
+// require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Register and Enqueue Styles.
+ */
+function edxstarter_register_styles() {
+
+	$theme_version = wp_get_theme()->get( 'Version' );
+
+	wp_enqueue_style( 'edxstarter-style', get_stylesheet_uri(), array(), $theme_version );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'edxstarter_register_styles' );
+
+
+/**
+ * Register and Enqueue Scripts.
+ */
+// function edxstarter_register_scripts() {
+
+// 	$theme_version = wp_get_theme()->get( 'Version' );
+
+// 	if ( ( ! is_admin() ) && is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+// 		wp_enqueue_script( 'comment-reply' );
+// 	}
+
+// 	wp_enqueue_script( 'edxstarter-js', get_template_directory_uri() . '/assets/js/index.js', array(), $theme_version, false );
+// 	wp_script_add_data( 'edxstarter-js', 'async', true );
+
+// }
+
+// add_action( 'wp_enqueue_scripts', 'edxstarter_register_scripts' );
+
+/**
+ * Fix skip link focus in IE11.
+ *
+ * This does not enqueue the script because it is tiny and because it is only for IE11,
+ * thus it does not warrant having an entire dedicated blocking script being loaded.
+ *
+ * @link https://git.io/vWdr2
+ */
+function edxstarter_skip_link_focus_fix() {
+	// The following is minified via `terser --compress --mangle -- assets/js/skip-link-focus-fix.js`.
+	?>
+	<script>
+	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+	</script>
+	<?php
+}
+add_action( 'wp_print_footer_scripts', 'edxstarter_skip_link_focus_fix' );
+
+
+/**
+ * Register navigation menus uses wp_nav_menu in five places.
+ */
+function edxstarter_menus() {
+
+	$locations = array(
+		'main'    => __( 'Main Top Menu', 'edxstarter' ),
+		'sidebar' => __( 'Sidebar Menu', 'edxstarter' ),
+		'footer'  => __( 'Footer Menu', 'edxstarter' ),
+		'social'  => __( 'Social Menu', 'edxstarter' ),
+	);
+
+	register_nav_menus( $locations );
+}
+
+add_action( 'init', 'edxstarter_menus' );
+
+
+if ( ! function_exists( 'wp_body_open' ) ) {
+
+	/**
+	 * Shim for wp_body_open, ensuring backwards compatibility with versions of WordPress older than 5.2.
+	 */
+	function wp_body_open() {
+		do_action( 'wp_body_open' );
+	}
+}
+
+
+/**
+ * Register widget areas.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function edxstarter_sidebar_registration() {
+
+	// Arguments used in all register_sidebar() calls.
+	$shared_args = array(
+		'before_title'  => '<h2 class="widget-title subheading heading-size-3">',
+		'after_title'   => '</h2>',
+		'before_widget' => '<div class="widget %2$s"><div class="widget-content">',
+		'after_widget'  => '</div></div>',
+	);
+
+	// Sidebar.
+	register_sidebar(
+		array_merge(
+			$shared_args,
+			array(
+				'name'        => __( 'Sidebar', 'edxstarter' ),
+				'id'          => 'sidebar-1',
+				'description' => __( 'Widgets in this area will be displayed in the sidebar.', 'edxstarter' ),
+			)
+		)
+	);
+
+	// Footer.
+	register_sidebar(
+		array_merge(
+			$shared_args,
+			array(
+				'name'        => __( 'Footer', 'edxstarter' ),
+				'id'          => 'sidebar-footer',
+				'description' => __( 'Widgets in this area will be displayed in the footer.', 'edxstarter' ),
+			)
+		)
+	);
+
+}
+
+add_action( 'widgets_init', 'edxstarter_sidebar_registration' );
